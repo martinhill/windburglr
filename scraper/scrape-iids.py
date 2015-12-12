b8be4188de663d62482375b1ada47ff2fabfcdc1
Martin Hill <martin.hill@advisorstream.com>
2015-12-11 21:50:20 -0500

    post observation to windburglr webapp REST endpoint

--- scraper/scrape-iids.py
+++ scraper/scrape-iids.py
@@ -4,24 +4,24 @@ import os
 import sys
 try:
     from bs4 import BeautifulSoup
-except ImportError, ie:
+except ImportError:
     sys.path.append(os.path.join(os.getcwd(), 'site-packages'))
     from bs4 import BeautifulSoup
-import urllib2
 import re
 import time
 from datetime import datetime
-import psycopg2
+# import psycopg2
 import json
-import urlparse
+import urllib
 import requests
+import argparse
 
 url_base = "http://atm.navcanada.ca/atm/iwv/"
 station_default = 'CYTZ'
 default_db = 'postgres://localhost'
 endopint_default = 'http://windburglr.aws.af.cm/wind'
 
-refresh_rate_default = 60
+refresh_rate_default = 58
 socket_timeout = 15
 
 wind_dir_re = re.compile('Wind Direction:')
@@ -30,49 +30,58 @@ wind_gust_re = re.compile('Gusting:')
 updated_re = re.compile('Updated:')
 error_time_fmt = '%m-%d %H:%M:%S'
 
-value_lookup = { '':None, 'CALM':0, '?':None, '--':None }
-coerce_int = lambda x: value_lookup[x] if value_lookup.has_key(x) else int(x)
+value_lookup = {'': None, 'CALM': 0, '?': None, '--': None}
+
+
+def coerce_int(x):
+    try:
+        return value_lookup[x]
+    except KeyError:
+        return int(x)
+
 
 def scrapeIIDSWebView(url):
     "Returns the wind data as tuple (direction, speed, gust, datetime)"
-    response = urllib2.urlopen(url, None, socket_timeout)
-    html = response.read()
-    response.close()
-    soup = BeautifulSoup(html)
+    # response = urllib2.urlopen(url, None, socket_timeout)
+    response = requests.get(url)
+    # html = response.read()
+    # response.close()
+    soup = BeautifulSoup(response.text, "html.parser")
     wind_dir = None
     wind_dir_text = None
     wind_speed = None
     wind_speed_text = None
     wind_gust = None
     wind_gust_text = None
-    find_iids_text = lambda expr: soup.find_all(text=expr)[0].next_element.next_element.text.strip()
+    find_iids_text = lambda expr: soup.find_all(
+        text=expr)[0].next_element.next_element.text.strip()
 
     # Wind direction
     try:
         wind_dir_text = find_iids_text(wind_dir_re)
         wind_dir = coerce_int(wind_dir_text)
-    except ValueError, ex:
+    except ValueError as ex:
         sys.stderr.write('ValueError %s: wind_dir_text="%s"\n' % (str(ex), wind_dir_text))
-    
+
     # Wind speed
     try:
         wind_speed_text = find_iids_text(wind_speed_re)
         wind_speed = coerce_int(wind_speed_text)
-    except ValueError, ex:
+    except ValueError as ex:
         sys.stderr.write('ValueError %s: wind_speed_text="%s"\n' % (str(ex), wind_speed_text))
 
     # Wind gust
     try:
         wind_gust_text = find_iids_text(wind_gust_re)
         wind_gust = coerce_int(wind_gust_text.strip('G'))
-    except ValueError, ex:
+    except ValueError as ex:
         sys.stderr.write('ValueError %s: wind_gust_text="%s"\n' % (str(ex), wind_gust_text))
-    
+
     # Update date/time
     try:
         updated_text = find_iids_text(updated_re)
         updated = datetime.strptime(updated_text, '%Y-%m-%d %H:%M:%SZ')
-    except ValueError, ex:
+    except ValueError as ex:
         sys.stderr.write('ValueError %s: updated_text="%s"\n' % (str(ex), updated_text))
         updated = None
 
@@ -93,7 +102,8 @@ def post_observation(endpoint, obs):
         'speed_kts': obs[2],
         'gust_kts': obs[3],
         'update_time': obs[4].strftime('%Y-%m-%d %H:%M:%S'),
-        })
+    })
+    print(str(payload))
     r = requests.post(endpoint, data=payload, headers={"Content-type": "application/json"})
     r.raise_for_status()
     return r
@@ -102,37 +112,37 @@ def post_observation(endpoint, obs):
 def run(conn, station, endpoint, refresh_rate=60):
     "Loop indefinitely scraping the data and writing to the connection c"
     last_obs_time = None
-    c = conn.cursor()
+    # c = conn.cursor()
     while True:
         try:
             obs = scrapeIIDSWebView(url_base + station)
-        except Exception, ex:
+        except Exception as ex:
             sys.stderr.write('%s %s in scrapeIIDSWebView(%s): %s\n' %
-                (datetime.now().strftime(error_time_fmt), type(ex).__name__, 
-                url_base+station, str(ex)))
+                             (datetime.now().strftime(error_time_fmt), type(ex).__name__,
+                              url_base + station, str(ex)))
             time.sleep(refresh_rate)
         else:
             # Ensure the observation is new (check update time)
             if last_obs_time is None or obs[3] > last_obs_time:
                 if obs[0] is not None or obs[1] is not None:
                     try:
-                        writeObservation(c, (station,) + obs)
+                        # writeObservation(c, (station,) + obs)
                         post_observation(endpoint, (station,) + obs)
-                    except Exception, ex:
-                        sys.stderr.write('%s %s in writeObservation: %s\n' % 
-                            (datetime.now().strftime(error_time_fmt), 
-                            type(ex).__name__, str(ex)) )
+                    except Exception as ex:
+                        sys.stderr.write('%s %s in writeObservation: %s\n' %
+                                         (datetime.now().strftime(error_time_fmt),
+                                          type(ex).__name__, str(ex)))
                         sys.stderr.write('obs = %s\n' % str(obs))
-                        c.close()
-                        conn.rollback()
-                        c = conn.cursor()
+                        # c.close()
+                        # conn.rollback()
+                        # c = conn.cursor()
                     else:
-                        conn.commit()
+                        # conn.commit()
                         last_obs_time = obs[3]
                     finally:
                         time.sleep(refresh_rate)
                 else:
-                    print 'Skipping observation %s' % str(obs)
+                    print('Skipping observation %s' % str(obs))
                     time.sleep(refresh_rate / 2)
             else:
                 # sleep half the refresh time when we get a duplicate
@@ -142,19 +152,19 @@ def run(conn, station, endpoint, refresh_rate=60):
 def getdb():
     "Get the database connection. Auto-detects heroku, appfog, or local environment"
 
-# Try appfog 
+# Try appfog
     services = json.loads(os.environ.get("VCAP_SERVICES", "{}"))
     if services:
         try:
             creds = services['postgresql-9.1'][0]['credentials']
-        except KeyError, ke:
-            print >> sys.stderr, "VCAP_SERVICES = %s" % str(services)
+        except KeyError as ke:
+            print("VCAP_SERVICES = %s" % str(services))
             raise ke
         database = creds['name']
         username = creds['username']
         password = creds['password']
         hostname = creds['hostname']
-        port     = creds['port']
+        port = creds['port']
 
         uri = "postgres://%s:%s@%s:%d/%s" % (
             creds['username'],
@@ -164,16 +174,16 @@ def getdb():
             creds['name'])
     else:
 # Try heroku / localhost
-        urlparse.uses_netloc.append("postgres")
+        # urlparse.uses_netloc.append("postgres")
         uri = os.environ.get("DATABASE_URL", default_db)
-        url = urlparse.urlparse(uri)
+        url = urllib.parse.urlparse(uri)
         database = url.path[1:]
         username = url.username
         password = url.password
         hostname = url.hostname
-        port     = url.port
+        port = url.port
 
-    print >> sys.stderr, "Connecting to database: %s" % uri
+    print("Connecting to database: %s" % uri)
 
     return psycopg2.connect(
         database=database,
@@ -190,8 +200,18 @@ def init_db(db):
         db.cursor().execute(f.read())
     db.commit()
 
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-t', '--target', type=str, help='target endpoint', default=endopint_default)
+    args = parser.parse_args()
+
+    # conn = getdb()
+    # init_db(conn)
+    run(None, station_default, args.target)
+    # conn.close()
+
+
 if __name__ == '__main__':
-    conn = getdb()
-    init_db(conn)
-    run(conn, station_default, endopint_default)
-    conn.close()
+    main()
